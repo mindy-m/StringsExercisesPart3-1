93b1ccdb7d4cc863e46d547571a33d41af01c329
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:24:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# ---- Debugging Practice ----
+
+# 1.  Assign your favorite number and word to the two variables.
+my_num = 0
+my_word = ''
+
+# a) Use format() and index values to print the string, "Here is my number: ___, and here is my word: ___, and here is my number again: ___."
+
+
+# b) Print the string, "Here is my word 3 times: ___/___/___, and here is my number squared: ___."
+
